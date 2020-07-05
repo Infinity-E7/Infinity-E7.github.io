82d73c45c361a817d733e528b33e44fec2f4ae12
Infinity-E7 <Infinity-E7@users.noreply.github.com>
2020-07-05 23:00:07 +0200

    Updates

---
+++ precache-manifest.ed39beb580459a4aa6a3f3097600a5eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "876e5ca82480864247463c65b090addf",
+    "url": "/victory-compendium/index.html"
+  },
+  {
+    "revision": "4f830125f20291ac055d",
+    "url": "/victory-compendium/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "3ef8a425f27932afcff7",
+    "url": "/victory-compendium/static/js/2.749b729c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/victory-compendium/static/js/2.749b729c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f830125f20291ac055d",
+    "url": "/victory-compendium/static/js/main.12c93759.chunk.js"
+  },
+  {
+    "revision": "51c7b4838c62c565b1fe",
+    "url": "/victory-compendium/static/js/runtime-main.0d1d96bb.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/victory-compendium/static/media/logo.ee7cd8ed.svg"
+  }
+]);
